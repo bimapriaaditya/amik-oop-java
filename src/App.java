daa6d1948457c13e86c46f0e0172663632964f1d
bimapriaaditya <bpabima@gmail.com>
2023-03-11 11:08:58 +0700

    Java Scanner & JOptions

--- src/App.java
+++ src/App.java
@@ -1,12 +1,18 @@
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         // Aritmatika lorem = new Aritmatika();
         // lorem.main();
 
-        DiskonHarga Tugas1 = new DiskonHarga();
-        Tugas1.main();
+        // DiskonHarga Tugas1 = new DiskonHarga();
+        // Tugas1.main();
+
+        // InputScanner inputScanner = new InputScanner();
+        // inputScanner.main();
+
+        JOptions jopt = new JOptions();
+        jopt.main();
 
     }
 }
