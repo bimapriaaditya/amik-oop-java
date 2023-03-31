178a64ee0dddac5c92035661a1efa84366733b3e
bimapriaaditya <bpabima@gmail.com>
2023-03-31 22:01:35 +0700

    cleanUp workspace

--- src/App.java
+++ src/App.java
@@ -2,23 +2,8 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Aritmatika lorem = new Aritmatika();
-        // lorem.main();
-
-        // DiskonHarga Tugas1 = new DiskonHarga();
-        // Tugas1.main();
-
-        // InputScanner inputScanner = new InputScanner();
-        // inputScanner.main();
-
-        // JOptions jopt = new JOptions();
-        // jopt.main();
-
-        // IfSelection ifs = new IfSelection();
-        // ifs.SwitchCaseStatement();
-
-        ForDo looping = new ForDo();
-        looping.DoWhileLoop();
+        HelloJava greetings = new HelloJava();
+        System.out.println(greetings.main());
 
     }
 }
